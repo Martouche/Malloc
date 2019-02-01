abeb71b828bca3db2db56fe2514ecce84771c374
FOUCOU <thibault.foucou@epitech.eu>
2019-02-01 10:24:38 +0100

    remove commentaries

--- src/malloc.c
+++ src/malloc.c
@@ -10,48 +10,11 @@
 header_t *head = NULL;
 header_t *tail = NULL;
 
-/*header_t *get_free_block(size_t size)
-{
-	header_t *curr;
-
-	while (curr) {
-		if (curr->is_free != 0 && curr->size >= size)
-			return curr;
-		curr = curr->next;
-	}
-	return NULL;
-}
-
-void *my_malloc(size_t size)
-{
-	void *block;
-    header_t *header;
-
-    block = sbrk(size);
-    if (!size)
-        return NULL;
-    if (block == (void*) -1) {
-        return NULL;
-    }
-	header = get_free_block(size);
-    if (header) {
-        header->is_free = 0;
-        return (void*)(header + 1);
-    }
-	return block;
-}
-
-void show_alloc_mem()
-{
-
-}
-*/
-
 void *malloc(size_t size)
 {
     void *p;
 
-    p = sbrk (0);
+    p = sbrk(0);
     if (sbrk(size) == (void*) -1)
         return NULL;
     return p;
